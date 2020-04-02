68d79af02fa68188df728877c4583de20152e32f
karen <karen.kim@roumit.com>
2020-04-02 18:34:41 +0900

    2차 레이아웃 퍼블리싱
    2차 레이아웃 펍블리싱

---
+++ index.js
@@ -0,0 +1,22 @@
+const domList = {
+  planWrap: document.querySelector('#planWrap'),
+  designWrap: document.querySelector('#designWrap'),
+  publishingWrap: document.querySelector('#publishingWrap')
+}
+
+const toggleList = (element) => {    
+  domList[element].classList.toggle('on');
+}
+
+const addList = (form) => {
+  const values = form.querySelector('input')
+  return false
+}
+
+const removeList = () => {
+
+}
+
+const updateList = () =>{
+
+}
