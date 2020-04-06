546ca42c5e1aee5e21be0c21ddea2f2542a9c2d3
karen <karen.kim@roumit.com>
2020-04-06 16:43:03 +0900

    리스트 입력시 null 방지, 숫자 추가

--- index.js
+++ index.js
@@ -4,22 +4,25 @@ const domList = {
   publishingWrap: document.querySelector('#publishingWrap'),
   planeForm: document.querySelector('#planeForm'), 
   planeTable: document.querySelector('#planeTable'), 
+  planeIndex: document.querySelector('#planeTable').children.length, 
 }
-
+ 
 const toggleList = (element) => {    
   domList[element].classList.toggle('on');
 }
 
 planeForm.onsubmit = (e)  =>{
   e.preventDefault();
-  addList(e.target, 'planeTable')
+  addList(e.target, 'planeTable');
 }
 
-const addList = (ele, tbody) => { 
-  const data = createObj(ele);
-  const list = createList(data);
-  domList[tbody].append(list);
-  return resetInput(ele);
+const addList = (ele, type) => { 
+  if(!checktInput(ele)) {
+    const data = createObj(ele);
+    const list = createList(data, domList[type]);
+    domList[type].prepend(list);
+    resetInput(ele);
+  }
 }
 
 const removeList = () => {
@@ -36,18 +39,37 @@ const createObj = (ele) =>{
   children.forEach(element => {
     dataObj[element.name] = element.value;
   });
-  return dataObj
+  return dataObj;
 }
 
-const createList = (data) => {
-  const tr = document.createElement('tr');
-  Object.keys(data).forEach(d => {
+const createList = (data, ele) => {
+  const th = ['check', 'no', 'date', 'version', 'writer', 'pages', 'text', 'classification'];
+  const tr = document.createElement('tr'); 
+  const check = document.createElement('input'); 
+
+  check.className = 'checkList';
+  check.type = 'checkbox'; 
+  data['check'] = check; 
+
+  data['no'] = ele.children.length + 1; 
+   
+  th.forEach(d => {
     const td = document.createElement('td');
+
     td.className = `list ${d}`;
-    td.textContent = data[d];
-    tr.append(td)
+
+    if(typeof data[d] === 'object') {
+      td.append(data[d]);
+    } 
+
+    if(typeof data[d] === 'string' || typeof data[d] === 'number') {
+      td.textContent = data[d];
+    } 
+
+    tr.append(td);
   })
-  return tr
+
+  return tr                 
 }
 
 const resetInput = (ele) =>{
@@ -56,4 +78,16 @@ const resetInput = (ele) =>{
     element.value = '';
   });
   return
+}
+
+const checktInput = (ele) =>{
+  const children = [...ele.querySelector('.dataField').children];
+  const trans = {date: '날짜', version: '버전', writer: '작성자', pages: '페이지', text: '내용', classification: '분류'}
+  return children.some(element => {
+    if(element.value === '') { 
+      alert(`${trans[element.name]}를 입력해주세요.`);
+    }
+    return element.value === '' 
+  });
+  
 }
