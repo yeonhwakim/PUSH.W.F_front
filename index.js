cdf8d72f200b769bba2d861200e693d15202de83
karen <karen.kim@roumit.com>
2020-04-08 18:54:58 +0900

    리스트 삭제하기 체크된 리스트 인덱스  가져오고 해당 인덱스의 리스트 를 지우고 나머지 인덱스 수정해주 resetListIndex

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const addList = (ele) => {
   if(!checktInput(ele)) {
     const type = domList['domList'].filter(l => {
       return !ele.id.indexOf(l) 
-    })[0]
+    })[0];
     const data = createObj(ele);   
     appendList(data, type);
     resetInput(ele);
@@ -50,12 +50,12 @@ const addList = (ele) => {
 const createObj = (ele) =>{ 
   const children = [...ele.querySelectorAll('.dataField')].reverse(); 
   const data = children.map(element => { 
-    const obj = {} 
-    const input = [...element.children]
+    const obj = {};
+    const input = [...element.children];
     input.forEach(ele => {
       obj[ele.name] = ele.value;
     })
-    return obj
+    return obj;
   }); 
   return data;
 }
@@ -64,21 +64,21 @@ const createList = (data, ele) => {
   const tr = document.createElement('tr');  
   const index = ele.children.length + 1; 
   const type = domList['domList'].filter(l => {
-    return !ele.id.indexOf(l) 
+    return !ele.id.indexOf(l);
   })  
   tr.innerHTML=
-  `<td class="list check">`+
-    `<input onchange="putCheckedData(this, '${type}')" class="checkList" type="checkbox" data-no="${index}" data-date="${data.date}" data-version="${data.version}" data-writer="${data.writer}" data-pages="${data.pages}" data-text="${data.text}" data-classification="${data.classification}">`+
-  `</td>`+
-  `<td class="list no">${index}</td>`+
-  `<td class="list date">${data.date}</td>`+
-  `<td class="list version">${data.version}</td>`+
-  `<td class="list writer">${data.writer}</td>`+
-  `<td class="list pages">${data.pages}</td>`+
-  `<td class="list text">${data.text}</td>`+
-  `<td class="list classification">${data.classification}</td>`
+    `<td class="list check">`+
+      `<input onchange="putCheckedData(this, '${type}')" class="checkList" type="checkbox" data-no="${index}" data-date="${data.date}" data-version="${data.version}" data-writer="${data.writer}" data-pages="${data.pages}" data-text="${data.text}" data-classification="${data.classification}">`+
+    `</td>`+
+    `<td class="list no">${index}</td>`+
+    `<td class="list date">${data.date}</td>`+
+    `<td class="list version">${data.version}</td>`+
+    `<td class="list writer">${data.writer}</td>`+
+    `<td class="list pages">${data.pages}</td>`+
+    `<td class="list text">${data.text}</td>`+
+    `<td class="list classification">${data.classification}</td>`
 
-  return tr                 
+  return tr;               
 }
 
 const appendList = (data, type) => {    
@@ -91,7 +91,7 @@ const appendList = (data, type) => {
 const resetInput = (ele) =>{    
   const children = [...ele.querySelectorAll('.dataField')]; 
   children.forEach(element => {  
-    element.remove()
+    element.remove();
   });  
   return
 }
@@ -103,7 +103,7 @@ const checktInput = (ele) =>{
     if(element.name !== 'no' && element.value === '') { 
       alert(`${trans[element.name]}를 입력해주세요.`);
     }
-    return element.name !== 'no' && element.value === '' 
+    return element.name !== 'no' && element.value === ''; 
   });
   
 }
@@ -116,8 +116,8 @@ const putCheckedData = (ele, type) => {
 }
 
 const getCheckedData = (type) => {      
-  const checkedList = domList[`${type}Hidden`].value.split(',');  
-  const children = domList[`${type}Table`].children;
+  const checkedList = document.querySelector(`#${type}Hidden`).value.split(',');  
+  const children = document.querySelector(`#${type}Table`).children;
   const list = checkedList.sort().map(index => {   
     const dataset = children[children.length - parseInt(index)].querySelector('input').dataset;
     return { 
@@ -149,9 +149,9 @@ const addInput = (type, data) => {
 }
 
 const moveList = (type) => {
-  const list = getCheckedData(type);
-  console.log(list)
+  const list = getCheckedData(type); 
   appendList(list, `${domList['domNext'][type]}`);
+  resetIndex(type);
 }
 
 const getList = (type) => { 
@@ -159,15 +159,15 @@ const getList = (type) => {
   resetInput(document.querySelector(`#${type}Form`));
   list.forEach(l  => {
     addInput(type, l);
-  })
+  });
+  resetIndex(type);
 }
 
 const updateList = (type) => {  
-  const data = createObj(document.querySelector(`#${type}Form`)); 
-  console.log(data)
-  const list = [...document.querySelector(`#${type}Table`).children]
+  const data = createObj(document.querySelector(`#${type}Form`));  
+  const list = [...document.querySelector(`#${type}Table`).children];
   data.forEach(d => {
-    const tr = list[list.length - parseInt(d.no)] 
+    const tr = list[list.length - parseInt(d.no)];
     tr.innerHTML=
       `<td class="list check">`+
         `<input onchange="putCheckedData(this, '${type}')" class="checkList" type="checkbox" data-no="${d.no}" data-date="${d.date}" data-version="${d.version}" data-writer="${d.writer}" data-pages="${d.pages}" data-text="${d.text}" data-classification="${d.classification}">`+
@@ -182,4 +182,28 @@ const updateList = (type) => {
   })
   resetInput(document.querySelector(`#${type}Form`));
   addInput(type); 
+}
+
+const deleteList = (type) => { 
+  const index = domList[`${type}Hidden`].value.split(',');   
+  const list = [...document.querySelector(`#${type}Table`).children];
+
+  index.forEach(i => {
+    return list[i -1].remove();
+  })
+
+  resetIndex(type); 
+  resetListIndex(type)
+}
+
+const resetIndex = (type) => {
+  document.querySelector(`#${type}Hidden`).value = '';  
+}
+
+const resetListIndex = (type) => {
+  const list = [...document.querySelector(`#${type}Table`).children]; 
+  const listLength = list.length;
+  list.forEach((l, index) => {
+    l.children[1].textContent = listLength - index;
+  })
 }
